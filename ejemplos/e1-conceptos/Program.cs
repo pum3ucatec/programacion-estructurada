dcb7931dbfcc50bd854f41ae53a31accd6a41f41
GastonQuelali <gaston_q@hotmail.com>
2025-05-15 20:08:01 -0400

    Primer ejemplo

---
+++ ejemplos/e1-conceptos/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+const double PI = 3.14159265358979323846;
+
+int a;
+int b;
+int suma;
+
+a = 3;
+b = 4;
+suma = a + b;
+
+Console.WriteLine("La suma de " + a + " + " + b + " es: " + suma);
+
+Console.WriteLine("Esta es la constate PI que tiene el valor " + PI);
+
+Console.WriteLine("Wilstermann campeon de la Libertadores 1994");
