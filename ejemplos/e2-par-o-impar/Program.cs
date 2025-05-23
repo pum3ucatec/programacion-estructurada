95c054d88f11abd257603f75fd31460e5ed35d56
GastonQuelali <gaston_q@hotmail.com>
2025-05-22 20:19:20 -0400

    Agregando el ejmplo 2

---
+++ ejemplos/e2-par-o-impar/Program.cs
@@ -0,0 +1,16 @@
+﻿// Dado un numero entero, determinar si es par o impar
+
+Console.WriteLine("Ingrese un numero entero:");
+
+string entradaPorTeclado = Console.ReadLine();
+
+int numero = int.Parse(entradaPorTeclado);
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("El numero es par");
+}
+else
+{
+    Console.WriteLine("El numero es impar");
+}
