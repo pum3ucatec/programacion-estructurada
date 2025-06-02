d91968c17cc7cf395aba1e5941a9ad310dda8c60
GastonQuelali <gaston_q@hotmail.com>
2025-06-02 23:48:30 +0000

    Utilizando el swtich con rangos

---
+++ ejemplos/e20-randgo-de-notas/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Ingrese una nota entre (0 - 100): ");
+string leerPorPantalla = Console.ReadLine();
+
+int nota = int.Parse(leerPorPantalla);
+
+string resultado;
+
+switch (nota)
+{
+    case int _ when nota >= 97 && nota <= 100:
+        resultado = "A+";
+        break;
+    case int _ when nota >= 93 && nota <= 96:
+        resultado = "A";
+        break;
+    case int _ when nota >= 90 && nota <= 92:
+        resultado = "A-";
+        break;
+    case int _ when nota >= 87 && nota <= 89:
+        resultado = "B+";
+        break;
+    case int _ when nota >= 83 && nota <= 86:
+        resultado = "B";
+        break;
+    case int _ when nota >= 80 && nota <= 82:
+        resultado = "B-";
+        break;
+    case int _ when nota >= 70 && nota <= 79:
+        resultado = "C";
+        break;
+    case int _ when nota >= 60 && nota <= 69:
+        resultado = "D";
+        break;
+    case int _ when nota >= 0 && nota < 60:
+        resultado = "F";
+        break;
+    default:
+        resultado = "Nota inválida";
+        break;
+}
+
+Console.WriteLine("La nota es: " + resultado);
