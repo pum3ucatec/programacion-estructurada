0b629dcf43a060ce48ce3648fff5620163e69536
GastonQuelali <gaston_q@hotmail.com>
2025-06-10 23:32:08 +0000

    Arreglos y matrices

---
+++ ejemplos/e40-arreglos-matrices/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+
+        Console.WriteLine("Elementos del arreglo:");
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            Console.WriteLine($"Índice {i}: {numeros[i]}");
+        }
+
+        // Matriz
+
+        int[,] matriz = {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        Console.WriteLine("Elementos de la matriz:");
+        for (int fila = 0; fila < 3; fila++)
+        {
+            for (int columna = 0; columna < 3; columna++)
+            {
+                Console.Write(matriz[fila, columna] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
